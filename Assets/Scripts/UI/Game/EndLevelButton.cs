730eef90ca5614809192af6da1edc2f3123908bc
Vladislav <GimliVlad@list.ru>
2024-03-13 10:16:49 +0300

    Add end level button

---
+++ Assets/Scripts/UI/Game/EndLevelButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(SceneLoader))]
+public class EndLevelButton : MonoBehaviour
+{
+    private Button _button;
+    private SceneLoader _sceneLoader;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _sceneLoader = GetComponent<SceneLoader>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(FinishLevel);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(FinishLevel);
+    }
+
+    private void FinishLevel()
+    {
+        PlayerPrefs.SetInt(GameSaver.Level, PlayerPrefs.GetInt(GameSaver.Level, GameSaver.LevelStep) + GameSaver.LevelStep);
+        PlayerPrefs.Save();
+        _sceneLoader.Load();
+    }
+}
