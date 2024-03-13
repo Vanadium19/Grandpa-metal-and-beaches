f7c5fa6543f90e0af3c7dd387f0452327ae431ff
Vladislav <GimliVlad@list.ru>
2024-03-13 18:42:34 +0300

    Add money pointer swith

--- Assets/Scripts/GamePlay/Environment/ATM.cs
+++ Assets/Scripts/GamePlay/Environment/ATM.cs
@@ -1,20 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class ATM : MonoBehaviour
 {
+    [SerializeField] private GameObject _pointer;
     [SerializeField] private Dumpster _dumpster;
     [SerializeField] private SpawnZone _spawnZone;
     [SerializeField] private MoneyPool _moneyPool;
 
-    private void OnEnable()
-    {
-        _dumpster.ScrapCollected += GiveMoney;
-    }
+    private void OnEnable() => _dumpster.ScrapCollected += GiveMoney;
 
-    private void OnDisable()
-    {
-        _dumpster.ScrapCollected -= GiveMoney;
-    }
+    private void OnDisable() => _dumpster.ScrapCollected -= GiveMoney;
 
     private void GiveMoney(Scrap scrap)
     {
@@ -23,5 +19,13 @@ public class ATM : MonoBehaviour
         money.gameObject.SetActive(true);
         money.transform.position = _spawnZone.GetRandomPointInZone();
         money.SetValue(scrap.Info.Price);
+
+        _pointer.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.TryGetComponent(out Player player))
+            _pointer.SetActive(false);
     }
 }
