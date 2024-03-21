85c52042334fa52993de8f7d910ce957a097e2bd
Vladislav <GimliVlad@list.ru>
2024-03-21 10:45:30 +0300

    Remove scrap name

--- Assets/Scripts/GamePlay/Scrap/ScrapInfo.cs
+++ Assets/Scripts/GamePlay/Scrap/ScrapInfo.cs
@@ -6,10 +6,8 @@ public class ScrapInfo : ScriptableObject
     [SerializeField] private float _weight;
     [SerializeField] private float _price;
     [SerializeField] private int _rank;
-    [SerializeField] private string _name;
 
     public float Weight => _weight;
     public float Price => _price;
     public int Rank => _rank;
-    public string Name => _name;
 }
