573a5c28e9d3259d3c755cd8d8b0671132215e78
Vladislav <GimliVlad@list.ru>
2024-03-26 11:57:37 +0300

    Update focus tracker

--- Assets/My Assets/Scripts/MainUI/FocusTracker.cs
+++ Assets/My Assets/Scripts/MainUI/FocusTracker.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 internal class FocusTracker : MonoBehaviour
 {
+    private float _actualTimeScale;
+
+    private void Awake() => _actualTimeScale = 1f;
+
     private void OnEnable()
     {
         Application.focusChanged += OnInBackgroundChangeApp;
@@ -29,5 +33,11 @@ internal class FocusTracker : MonoBehaviour
 
     private void MuteAudio(bool value) => AudioListener.volume = value ? 0 : GameSaver.GetCurrentVolume();
 
-    private void PauseGame(bool value) => Time.timeScale = value ? 0 : 1;
+    private void PauseGame(bool value)
+    {
+        if (value)        
+            _actualTimeScale = Time.timeScale;
+
+        Time.timeScale = value ? 0 : _actualTimeScale;
+    }
 }
