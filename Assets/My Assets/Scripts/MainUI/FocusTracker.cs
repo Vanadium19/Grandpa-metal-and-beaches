36b4534fa8c86be75472cb383a7bdd00db7c5c5a
Vladislav <GimliVlad@list.ru>
2024-03-27 15:33:15 +0300

    Fix sound in focus tracker

--- Assets/My Assets/Scripts/MainUI/FocusTracker.cs
+++ Assets/My Assets/Scripts/MainUI/FocusTracker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class FocusTracker : MonoBehaviour
 {
     private float _currentTimeScale = 1f;
+    private float _currentVolume;
+
+    private void Awake() => _currentVolume = PlayerPrefs.GetFloat(GameSaver.Audio, GameSaver.DefaultVolume);
 
     private void OnEnable()
     {
@@ -19,6 +22,8 @@ public class FocusTracker : MonoBehaviour
 
     public void SetCurrentTimeScale(float value) => _currentTimeScale = value;
 
+    public void SetCurrentVolume(float value) => _currentVolume = value;
+
     private void OnInBackgroundChangeApp(bool inApp)
     {
         MuteAudio(!inApp);
@@ -31,7 +36,7 @@ public class FocusTracker : MonoBehaviour
         PauseGame(isBackground);
     }
 
-    private void MuteAudio(bool value) => AudioListener.volume = value ? 0 : GameSaver.GetCurrentVolume();
+    private void MuteAudio(bool value) => AudioListener.volume = value ? 0 : _currentVolume;
 
     private void PauseGame(bool value) => Time.timeScale = value ? 0 : _currentTimeScale;
 }
--- Assets/My Assets/Scripts/Shop/Advertising/VideoAd.cs
+++ Assets/My Assets/Scripts/Shop/Advertising/VideoAd.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 internal class VideoAd : MonoBehaviour
 {
+    private readonly float _pauseTimeScale = 0f;
+    private readonly float _playTimeScale = 1f;
+    private readonly float _minVolume = 0;
+
+    [SerializeField] private FocusTracker _focusTracker;
+
     private float _money;
     private Wallet _wallet;
 
@@ -14,16 +20,24 @@ internal class VideoAd : MonoBehaviour
 
     private void OnOpenCallback()
     {
-        Time.timeScale = 0;
-        AudioListener.volume = 0;
+        AudioListener.volume = _minVolume;
+        _focusTracker.SetCurrentVolume(_minVolume);
+
+        Time.timeScale = _pauseTimeScale;
+        _focusTracker.SetCurrentTimeScale(_pauseTimeScale);
     }
 
     private void OnRewardCallback() => _wallet.AddMoney(_money);
 
     private void OnCloseCallback()
     {
-        Time.timeScale = 1;
-        AudioListener.volume = GameSaver.GetCurrentVolume();
+        var volume = PlayerPrefs.GetFloat(GameSaver.Audio, GameSaver.DefaultVolume);
+
+        AudioListener.volume = volume;
+        _focusTracker.SetCurrentVolume(volume);
+
+        Time.timeScale = _playTimeScale;
+        _focusTracker.SetCurrentTimeScale(_playTimeScale);
         gameObject.SetActive(false);
     }   
 }
