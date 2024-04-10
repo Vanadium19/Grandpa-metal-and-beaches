b7be3ba869169453a43cb04b3e155547eaee9eb0
Vladislav <GimliVlad@list.ru>
2024-04-10 10:51:48 +0300

    New shop folder + advertising button

---
+++ Assets/My Assets/Scripts/Shop/UI/UpgradePanel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UpgradePanel : MonoBehaviour
+{
+    public event UnityAction PlayerUpgraded;
+
+    private void OnDisable() => PlayerUpgraded?.Invoke();
+}
