6631cc4af8f924d187037b4c890f85118a116374
Vladislav <GimliVlad@list.ru>
2024-04-10 11:32:23 +0300

    Change leaderboard ui

--- Assets/My Assets/Scripts/Menu/Leaderboard/LeaderboardElement.cs
+++ Assets/My Assets/Scripts/Menu/Leaderboard/LeaderboardElement.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 
 internal class LeaderboardElement : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _info;
+    [SerializeField] private TMP_Text _rank;
+    [SerializeField] private TMP_Text _name;
+    [SerializeField] private TMP_Text _score;
 
-    public void Initialize(string name, int rank, int score) => _info.text = $"{rank}. {name} {score}";
+    public void Initialize(string name, int rank, int score)
+    {
+        _rank.text = $"{rank}.";
+        _name.text = name;
+        _score.text = score.ToString();
+    }
 }
