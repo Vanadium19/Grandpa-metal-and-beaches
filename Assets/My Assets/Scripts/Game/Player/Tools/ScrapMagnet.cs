2ba4c488a1707de11698504e1c80c50451d919d0
Vladislav <GimliVlad@list.ru>
2024-04-11 20:45:48 +0300

    Change scrap mahnet value

--- Assets/My Assets/Scripts/Game/Player/Tools/ScrapMagnet.cs
+++ Assets/My Assets/Scripts/Game/Player/Tools/ScrapMagnet.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 internal class ScrapMagnet : MonoBehaviour
 {
-    private readonly float _delay = 2f;
+    private readonly float _delay = 1f;
 
     private readonly float _minDistance = 0f;
     private readonly float _maxDistance = 0.5f;
@@ -18,7 +18,7 @@ internal class ScrapMagnet : MonoBehaviour
         float elapsedTime = 0;
         float _range = Random.Range(_minDistance, _maxDistance);
 
-        while (Vector3.Distance(transform.position, scrap.position) > _range)
+        while (Vector3.Distance(transform.position, scrap.position) >= _range)
         {
             position = Vector3.MoveTowards(scrap.position, transform.position, _speed * Time.deltaTime);
             scrap.position = elapsedTime >= _delay ? transform.position : position;
