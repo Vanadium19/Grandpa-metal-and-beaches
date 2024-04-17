1e7342e1a981e2b67868dcdb969d00d9ea9c3107
Vladislav <GimliVlad@list.ru>
2024-04-17 13:41:09 +0300

    Add closable panels in tutorial

--- Assets/My Assets/Scripts/Game/Level/UI/Tutorial.cs
+++ Assets/My Assets/Scripts/Game/Level/UI/Tutorial.cs
@@ -6,6 +6,7 @@ internal class Tutorial : MonoBehaviour
     [SerializeField] private string _name;
     [SerializeField] private Menu _menu;
     [SerializeField] private GameObject _tutorialPanel;
+    [SerializeField] private GameObject[] _closablePanels;
 
     private void Start()
     {
@@ -17,10 +18,20 @@ internal class Tutorial : MonoBehaviour
 
     private void TurnOnTutorial()
     {
+        ClosePanels();
         _tutorialPanel.SetActive(true);
         _menu.StopTime();
 
         PlayerPrefs.SetInt(GameSaverData.Tutorial + _name, Convert.ToInt16(true));
         PlayerPrefs.Save();
+    }
+
+    private void ClosePanels()
+    {
+        if (_closablePanels.Length == 0)        
+            return;        
+
+        foreach (var closablePanel in _closablePanels)        
+            closablePanel.SetActive(false);        
     }
 }
