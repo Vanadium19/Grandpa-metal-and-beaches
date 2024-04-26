d3cf3798ca934d67aca5020a47245f008d083113
Vladislav <GimliVlad@list.ru>
2024-04-26 11:08:42 +0300

    Add whitespace

--- Assets/My Assets/Scripts/Menu/Settings/Localizer.cs
+++ Assets/My Assets/Scripts/Menu/Settings/Localizer.cs
@@ -8,7 +8,7 @@ internal class Localizer : MonoBehaviour
     public const string EnglishCode = "English";
     public const string TurkishCode = "Turkish";
     public const string Russian = "ru";
-    public const string English= "en";
+    public const string English = "en";
     public const string Turkish = "tr";
 
     private void Awake()
